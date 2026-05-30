0671f5d235fc99c682726eaebec467abc5a35e63
abolfazl <abolfazlhadinjad.h.n.k@gmail.com>
2026-05-30 18:19:13 +0330

    dockerization

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite"; // دقت کنید: این برای نسخه 4 ضروری است
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: true,
+    port: 5173,
+  },
 });

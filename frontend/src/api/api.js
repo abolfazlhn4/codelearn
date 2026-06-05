55eda491086f0a883ea2fc637eeff4a9b0dd1f79
abolfazlhn4 <eaabolfazlhn@gmail.com>
2026-06-05 11:42:49 +0330

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_URL || "http://localhost:8000";
+const baseURL = import.meta.env.VITE_API_URL;
+//const baseURL = "http://localhost:8000";
 
 const api = axios.create({
   baseURL: baseURL,
